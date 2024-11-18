7648b1e36c6d0cdb85ad06a8b436d36b7a45d6bb
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-18 15:03:36 +0100

    chore: Remove redundant config options
    Remove unnecessary charset definition from Sequelize
    configuration. This simplifies the configuration file
    and has no impact on application functionality.

--- config/config.js
+++ config/config.js
@@ -27,9 +27,6 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
             rejectUnauthorized: false,
         },
     },
-    define: {
-        charset: 'utf8mb4', 
-    },
 });
 
 // Tester la connexion
