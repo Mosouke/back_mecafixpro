7713038604a55fbca5ac8743be5ec64bed9b32ec
Mosouke <nicolas.barboux@lapalateforme.io>
2024-11-18 13:53:34 +0100

    fix: Allow database schema updates
    Enable automatic database schema updates on application
    startup to accommodate future changes.  This prevents
    potential conflicts and ensures data compatibility.

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ async function initRoles() {
  */
 async function initApp() {
     try {
-        await sequelize.sync({ force: false, alter: false });
+        await sequelize.sync({ force: false, alter: true });
 
         await initRoles();
 
