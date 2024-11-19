08de1711b213f425e1c9fff64d0b3be34e8488f1
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-19 13:35:25 +0100

    feat: Enhance client creation process
    Improve client creation by preventing duplicate profiles
    for the same user and providing default values for
    optional fields.  Add error handling for duplicate
    client profiles.

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,54 +3,6 @@ const { Clients } = require('../Models');
 const { validationResult } = require('express-validator');
 
 /**
- * @module controllers/clientController
- */
-
-/**
- * Get all clients.
- * 
- * @function getClients
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @returns {Object} res - Response object containing all clients or an error message
- * @throws {Error} 500 - Internal server error if unable to fetch clients
- */
-exports.getClients = async (req, res) => {
-    try {
-        const clients = await Clients.findAll();
-        res.status(200).json(clients);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
-    }
-};
-
-/**
- * Get a specific client by ID.
- * 
- * @function getClient
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @returns {Object} res - Response object containing the client or an error message
- * @throws {Error} 404 - Client not found if the specified ID does not exist
- * @throws {Error} 500 - Internal server error if unable to fetch the client
- */
-exports.getClient = async (req, res) => {
-    try {
-        // Recuperation de l'id depuis le token (à partir de la requête) envoyé par le middleware auth
-        const { client_id } = req.user;
-        const client = await Clients.findOne({ where: { client_id } });
-        if (!client) {
-            return res.status(404).json({ message: 'Client not found' });
-        }
-        res.status(200).json(client);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
-    }
-};
-
-/**
  * Create a new client.
  * 
  * @function createClient
@@ -74,18 +26,29 @@ exports.createClient = async (req, res) => {
             return res.status(401).json({ message: 'User not authenticated' });
         }
 
-        const { client_image_name, client_name, client_last_name, client_phone_number, client_address } = req.body;
+        // Vérifier si le client existe déjà pour cet utilisateur
+        const existingClient = await Clients.findOne({
+            where: { fk_mail_user: userEmail }
+        });
 
+        if (existingClient) {
+            return res.status(400).json({ message: 'Client profile already exists for this user' });
+        }
+
+        // Données du client à créer
+        const { client_image_name = 'default_image.png', client_name, client_last_name, client_phone_number = '0000000000', client_address = 'Non renseignée' } = req.body;
+
+        // Créer le nouveau client
         const client = await Clients.create({
             client_image_name,
             client_name,
             client_last_name,
             client_phone_number,
             client_address,
-            fk_mail_user: userEmail 
+            fk_mail_user: userEmail  // Lier l'utilisateur au client par l'email
         });
 
-        res.status(201).json(client);
+        res.status(201).json(client);  // Retourner les informations du client créé
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: error.message });
