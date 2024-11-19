270748b5c9c66a80993f3cdf145ab36ff93f7d17
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-19 09:38:56 +0100

    fix: Restrict CORS origin to localhost
    Update CORS middleware to allow requests only from
    `http://localhost:5173`. This enhances security by
    preventing unauthorized access from other origins.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     next();
