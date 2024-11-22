67974828b21e729bd8dad9369ed4f39b80cb1726
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-22 11:37:13 +0100

    fix: Rename password field in registration
    Rename the password field in the user registration
    request to improve clarity and consistency.  This
    change ensures better code readability and
    maintains consistency with other password fields.

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ if (!JWT_SECRET) {
  * @param {Object} res - Réponse Express
  */
 exports.register = async (req, res) => {
-    const { mail_user_client, password_user_client, client_name, client_last_name, client_phone_number, client_address } = req.body;
+    const { mail_user_client, user_client_password, client_name, client_last_name, client_phone_number, client_address } = req.body;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -37,7 +37,7 @@ exports.register = async (req, res) => {
         // Création de l'utilisateur
         const newUserClient = await UsersClients.create({
             mail_user_client,
-            password_user_client: hashedPassword,
+            user_client_password: hashedPassword,
             client_name,
             client_last_name,
             client_phone_number,
