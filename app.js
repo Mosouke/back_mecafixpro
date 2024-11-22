a96a22641b53fff6a37f9f70dd41a17f9db8885c
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-22 12:04:53 +0100

    chore: Add CORS configuration
    Allow requests from specified origins to prevent CORS errors.
    This ensures that the backend API can be accessed
    from the frontend application.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
+
 const allowedOrigins = [
     'https://front-react-mecafixpro.vercel.app', 
     'http://localhost:3000', 
