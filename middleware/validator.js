09df899587d430c3c9aa0ceb389801210503474e
Mosouke <nicolas.barboux@laplateforme.io>
2024-11-22 11:50:59 +0100

    refactor: Rename password field in validator
    Rename the `password` field to `user_client_password` for
    consistency and clarity.  This improves code readability
    and maintainability.

--- middleware/validator.js
+++ middleware/validator.js
@@ -12,7 +12,7 @@ const validateEmail = check('mail_user_client')
 /**
  * Validation réutilisable pour les mots de passe.
  */
-const validatePassword = check('password')
+const validatePassword = check('user_client_password')
     .isLength({ min: 4, max: 100 })
     .withMessage('Le mot de passe doit comporter entre 4 et 100 caractères');
 
