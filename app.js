bebb75f80698cbeb84d78a243b195c8690b7272f
Mosouke <nicolas.barboux@laplateforme.io>
2024-12-03 10:04:58 +0100

    fix: Prevent sequelize table alteration

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ async function initRoles() {
  */
 async function initApp() {
     try {
-        await sequelize.sync({ force: false, alter: true });
+        await sequelize.sync({ force: false, alter: false });
 
         await initRoles();
 
